f86fe376e4ad2978d249dac83e773937f01d746e
Ankit Bourasi <ankitbourasi0@gmail.com>
2023-08-13 16:25:55 +0530

    head to Head

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Provider from './components/Provider'
 import './globals.css'
 import {Poppins,Roboto} from"@next/font/google"
@@ -18,13 +19,13 @@ export default function RootLayout({ children }) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
 
-     <head>
+     <Head>
      <title>Instagram reel saver</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="By Team Samosaa" />
       <link rel="icon" href="/favicon.ico" />
       <script type='text/javascript' src='//pl20295130.highcpmrevenuegate.com/4c/b5/69/4cb569e94e5da8fb52417e472cce7745.js'></script>
-  </head>
+  </Head>
       
       <body className='overflow-x-hidden bg-white text-gray-900 dark:bg-neutral-900 dark:text-slate-100' >
         
