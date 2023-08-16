8fcddc02e90991d72d87e62ea9f8886c9dcff66c
Ankit Bourasi <ankitbourasi0@gmail.com>
2023-08-16 11:57:42 +0530

    ads updated native ads

--- app/components/AdComponent.js
+++ app/components/AdComponent.js
@@ -1,36 +1,63 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from "react";
 
 const AdComponent = () => {
-    const banner = useRef()
-   const atOptions = {
-	'key' : '69009e52dc685b7228ad44587f12faa3',
-		'format' : 'iframe',
-		'height' : 60,
-		'width' : 468,
-		'params' : {}
-	};
+
+  const [windowSize, setWindowSize] = useState([
+    typeof window !== "undefined" && window.innerWidth,
+    typeof window !== "undefined" && window.innerHeight,
+  ]);
+
   useEffect(() => {
-    if(banner.current && !banner.current.firstChild){
-        const conf = document.createElement('script')
-        const script = document.createElement('script')
-        script.type = 'text/javascript'
-        script.src = `//www.profitablecreativeformat.com/${atOptions.key}/invoke.js`
-        conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
+    const handleWindowResize = () => {
+      if (typeof window !== "undefined") {
+         setWindowSize([window.innerWidth , window.innerHeight]);
 
-        banner.current.append(conf)
-        banner.current.append(script)
-    }
-  }, [banner]);
-
-  
-
-  return(
+      }
+    };
     
-  <div className='mx-2 my-5  flex justify-center   items-center text-white text-center'>
-      <div className="mx-2 my-5  flex justify-center   items-center text-white text-center" ref={banner}></div>
-   <div className="mx-2 my-5  flex justify-center   items-center text-white text-center" ref={banner}></div>
+    window.addEventListener("resize", handleWindowResize);
 
-  </div>  )
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };   
+  }, []);
+
+  const banner = useRef();
+  const atOptions = {
+    key:   "69009e52dc685b7228ad44587f12faa3",
+    format: "iframe",
+    height: 60,
+    width:  468,
+    params: {},
+  };
+  useEffect(() => {
+    if (banner.current && !banner.current.firstChild) {
+      const conf = document.createElement("script");
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = `//www.profitablecreativeformat.com/${atOptions.key}/invoke.js`;
+      conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
+
+      banner.current.append(conf);
+      banner.current.append(script);
+    }
+  }, [banner,windowSize ]);
+
+  return (
+    <div className="mx-2 my-5  flex justify-center   items-center text-white text-center">
+      {
+        windowSize[0] > 420 && <>
+         <div
+            className="mx-2 my-5  flex justify-center   items-center text-white text-center"
+            ref={banner}
+          ></div>
+          <div
+            className="mx-2 my-5  flex justify-center   items-center text-white text-center"
+            ref={banner}
+          ></div></> 
+      }
+    </div>
+  );
 };
- 
+
 export default AdComponent;
